3339fdb51ed0e7ea160ecc1c8b829631bf75c43e
junayednoman <junayednoman05@gmail.com>
2024-04-19 11:24:12 +0600

    font problem solved

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,13 @@ import localFont from "next/font/local"
 import "./globals.css";
 
 const notoBengali = localFont({
-  src: "../../node_modules/next/font/local/NotoSansBengali-Regular.ttf",
+  src: "../../public/fonts/NotoSansBengali-Regular.ttf",
   display: "swap",
 });
+// const notoBengali = localFont({
+//   src: "../../node_modules/next/font/local/NotoSansBengali-Regular.ttf",
+//   display: "swap",
+// });
 
 export const metadata = {
   title: "প্রাথমিক বিদ্যালয়ের তথ্য ব্যবস্থাপনা সিস্টেম",
